b5c34d92421a7adc1b3884838e68e2ce1eec3c2a
konstantin <erenegaa136@gmail.com>
2025-09-14 15:39:53 +0300

    made get_date and finished code

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 """
 
 # from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card #,get_date
+from src.widget import mask_account_card,get_date
 
 
 def main() -> None:
     """Функция управления и вывода"""
     print("Банковский виджет - маскировка данных")
-    card = input("Введите наименование и номер карты: ")
+    card = input("Введите наименование карты или счет и номер: ")
+    enter_date = input("Введите дату в формате: '2024-03-11T02:26:18.671407': ")
     # account = input("Введите номер счёта: ")
     try:
         print(mask_account_card(card))
+        print(get_date(enter_date))
     except ValueError as e:
         print(f"Ошибка: {e}")
 
--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,41 @@
 import re
+from datetime import datetime
 
 
 def mask_account_card(account_number: str) -> str:
     """Функция, обрабатывающая тип карты или счета"""
-    # Убираем все лишние символы кроме цифр
-    text = re.sub(r"[^а-яА-Яa-zA-Z\s]", '', account_number, flags=re.UNICODE)
-    # Убираем все лишние символы кроме букв
+    # проверка на тип данных
+    if not isinstance(account_number, str):
+        raise ValueError("Нужно ввести строку")
+
+    # Убираем все буквы
+    text = re.sub(r"[^а-яА-ЯёЁa-zA-Z\s]", "", account_number, flags=re.UNICODE).strip()
+    text = re.sub(r"\s+", " ", text)
+
+    # Убираем все цифры
     digins = re.sub(r"\D", "", account_number)
 
-    # Проверка схожих символов для подставки типа операции
-    if re.search(r'[а-яА-Я]', account_number):
-        return str(text + masks.get_mask_card_number(int(digins)))
+    if not digins:
+        raise ValueError("Не найден номер карты или счета")
+
+    # Обработка запроса счета
+    if text.lower().startswith("счет") or text.lower().startswith("счёт"):
+        return f"{text}: **{digins[-4:]}"
+    # Обработка запроса номера карты
+    if len(digins) == 16:
+        text_display = text if text else "Карта"
+        return f"{text_display}: {digins[:4]} {digins[4:6]}** **** {digins[-4:]}"
+
+    raise (ValueError(
+        "Не верно введены данные: ожидается формат (тип карты и 16 цифр или пометка 'счет' и номер)"
+    ))
 
 
-# def get_date(new_date: str) -> str:
-#   """Функция меняет формат даты"""
-#    # срезами меняем формат даты
-#   return f"{new_date[8:10]} {new_date[5:7]} {new_date[:4]}"
+def get_date(date_str: str) -> str:
+    """Функция меняет формат даты"""
+    try:
+        dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+        # Задаем формат даты
+        return dt.strftime("%d.%m.%Y")
+    except ValueError:
+        raise ValueError("Неверный формат даты")
