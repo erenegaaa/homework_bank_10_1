3e042c2e54ac797653bfeabe3cd83d21eeae5df9
konstantin <erenegaa136@gmail.com>
2025-09-27 22:36:53 +0300

    Пройдены тесты и исправлены баги

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-from typing import List, Dict, Optional
+from typing import Dict, List, Optional
 
 
 def filter_by_state(date: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Функция фильтрует список словарей по параметру 'state'.
     state = 'EXECUTED' по умолчанию.
-    Функция возвращает отфильтрованный список
+    Функция возвращает отфильтрованный список.
     """
     return [item for item in date if item.get("state") == state]
 
@@ -20,13 +20,18 @@ def filter_by_state(date: List[Dict], state: str = "EXECUTED") -> List[Dict]:
 
 
 def sort_by_date(date_info: List[Dict], key: Optional[str] = "date", reverse: bool = True) -> List[Dict]:
-    return sorted(date_info, key=lambda x: x.get(key), reverse=reverse)
+    """
+    Функция сортирует список словарей по параметру 'date' на убывание.
+    Условие функции: параметр 'reverse' = True.
+    Возвращает отсортированный список по дате.
+    """
+    return sorted(date_info, key=lambda x: x.get(key) or "", reverse=reverse)
 
 
-data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    ]
-print(sort_by_date(data))
+# data = [
+#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+#     ]
+# print(sort_by_date(data))
