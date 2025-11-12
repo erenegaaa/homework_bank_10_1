5c2c5e4c95c63670294eb65a0465df79b32764b0
konstantin <erenegaa136@gmail.com>
2025-11-12 05:49:14 +0300

    Функционал чтения файлов

---
+++ src/financial_transactions.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from typing import Any, Hashable
+
+
+def read_transactions_csv(file_path: str) -> list[dict[Hashable, Any]]:
+    """
+    Считывает финансовые операции из CSV-файла и возвращает список словарей.
+    file_path: путь к CSV-файлу.
+    список словарей с транзакциями.
+    """
+    df = pd.read_csv(file_path, sep=";", encoding="utf-8")
+    return df.to_dict(orient="records")
+
+
+def read_transactions_excel(file_path: str) -> list[dict[Hashable, Any]]:
+    """
+    Считывает финансовые операции из Excel-файла и возвращает список словарей.
+    file_path: путь к Excel-файлу (.xlsx)
+    список словарей с транзакциями.
+    """
+    df = pd.read_excel(file_path)
+    return df.to_dict(orient="records")
